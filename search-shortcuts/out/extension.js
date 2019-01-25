ce62de9af885b20236aec0657d9eabf585d29c38
biziplz <uberud16@gmail.com>
2019-01-25 19:14:39 +0100

    rm out

--- search-shortcuts/out/extension.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-function activate({ subscriptions }) {
-    subscriptions.push(vscode.commands.registerCommand("extension.search-shortcuts", () => {
-    }));
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
